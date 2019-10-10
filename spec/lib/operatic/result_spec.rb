755c86e85a4e1e39190ed11a35f9afc64869ee54
Ben Pickles <spideryoung@gmail.com>
2019-10-10 13:53:04 +0100

    Allow a user-defined #data accessor on a Result.

--- spec/lib/operatic/result_spec.rb
+++ spec/lib/operatic/result_spec.rb
@@ -1,24 +1,24 @@
 RSpec.describe Operatic::Result do
   describe '.generate' do
-    let(:klass) { described_class.generate(:a, :b) }
+    let(:klass) { described_class.generate(:data, :stuff) }
     let(:result) { klass.new }
 
     it 'creates a Result subclass with specified data accessors' do
-      result.a = 1
-      result.b = 2
+      result.data = 1
+      result.stuff = 2
 
-      expect(result.a).to eql(1)
-      expect(result.b).to eql(2)
+      expect(result.data).to eql(1)
+      expect(result.stuff).to eql(2)
       expect(result).not_to respond_to(:c)
-      expect(result.data).to eql({ a: 1, b: 2 })
+      expect(result.to_hash).to eql({ data: 1, stuff: 2 })
 
-      result.data[:c] = 3
+      result.to_hash[:c] = 3
 
-      expect(result.data).to eql({ a: 1, b: 2, c: 3 })
+      expect(result.to_hash).to eql({ data: 1, stuff: 2, c: 3 })
     end
   end
 
-  describe '#failure' do
+  describe '#failure!' do
     let(:result) { described_class.new }
 
     it 'marks itself as a failure, sets data, and freezes itself' do
@@ -26,12 +26,12 @@ RSpec.describe Operatic::Result do
 
       expect(result).to be_failure
       expect(result).to be_frozen
-      expect(result.data).to eql({ a: 1, b: 2 })
-      expect(result.data).to be_frozen
+      expect(result.to_hash).to eql({ a: 1, b: 2 })
+      expect(result.to_hash).to be_frozen
     end
   end
 
-  describe '#success' do
+  describe '#success!' do
     let(:result) { described_class.new }
 
     it 'marks itself as a success, sets data, and freezes itself' do
@@ -39,8 +39,8 @@ RSpec.describe Operatic::Result do
 
       expect(result).to be_success
       expect(result).to be_frozen
-      expect(result.data).to eql({ a: 1, b: 2 })
-      expect(result.data).to be_frozen
+      expect(result.to_hash).to eql({ a: 1, b: 2 })
+      expect(result.to_hash).to be_frozen
     end
   end
 end
